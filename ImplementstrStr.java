87c19299fd64027e21da9886e889c2e1ebd4c1f8
SiamadMirzapour <sirvan39@gmail.com>
2022-05-18 12:31:36 +0430

    Create ImplementstrStr.java
    first solution

---
+++ ImplementstrStr.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+         if (haystack == null || needle == null)  return -1;
+         if (haystack.equals(needle)) return 0;        
+         int needleLength = needle.length();
+         for (int i = 0; i < haystack.length() - needleLength + 1; i++) {
+            if (haystack.substring(i, i + needleLength).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
